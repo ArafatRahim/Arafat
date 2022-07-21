4ba142692a99ebf5cb0f5464fa4feb3af4ae61c9
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-07-21 13:51:31 +0600

    Add files via upload

---
+++ aysg.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//using namspace std;
+int main()
+{
+    int A,B,X;
+    std::cout<< "Enter two number=";
+    std::cin >> A >>B;
+    X=A+B;
+    std:: cout<<A<<"+"<<B<<"="<<X;
+    return 0;
+
+}
---
+++ ssdb.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+double A,R,Pi;
+ cout<<"Enter the redious :";
+ cin>>R;
+ A=M_PI*R*R;
+cout <<R<<"*"<<R<<"*"<<Pi<<"="<<A;
+return 0;
+}
