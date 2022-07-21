8a7b4fb605abba89cab05e9b991da483d044a209
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-07-21 22:55:09 +0600

    Add files via upload

---
+++ simplesum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+using std::endl;
+int main()
+{
+    int A,B,sum;
+    cin >>A;
+    cin >>B;
+    sum=A+B;
+    cout <<"SOMA = "<<sum<<endl;
+
+    return 0;
+
+}
