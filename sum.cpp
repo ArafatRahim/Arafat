6767110f277daa1894071818f1e39a8829ea0ccd
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-07-21 21:40:29 +0600

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+    int A,B,X;
+    cout<< "Enter 1st number="<<endl;
+    cin >> A ;
+    cout<<"Enter 2nd number="<<endl;
+    cin>> B;
+    X=A+B;
+    cout<<"X = "<<X;
+    return 0;
+
+}
