05a14290eba833d5bc6c9f5527dc4de06a4f26ca
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-07-21 23:05:43 +0600

    Add files via upload

---
+++ mul.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+using std::endl;
+int main()
+{
+    int A,B,m;
+    cin >>A;
+    cin >>B;
+    m=A*B;
+    cout <<"PROD = "<<m<<endl;
+
+    return 0;
+
+}
