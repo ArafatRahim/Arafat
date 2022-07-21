e2c972882cb5de6fc1780e5b79fa59656b4d9a2d
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-07-21 22:40:43 +0600

    Add files via upload

--- ssdb.cpp
+++ ssdb.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
-#include<cmath>
+#include<iomanip>
 using namespace std;
-
+using std::endl;
 int main()
 {
-double A,R,Pi;
- cout<<"Enter the redious :";
- cin>>R;
- A=M_PI*R*R;
-cout <<R<<"*"<<R<<"*"<<Pi<<"="<<A;
-return 0;
+    double A,R,PI;
+    PI=3.14159;
+    cin>>R;
+    A=PI*R*R;
+    cout<<fixed<<setprecision(4);
+    cout<<"A="<<A<<endl;
+    return 0;
 }
