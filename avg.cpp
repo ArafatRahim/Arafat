31bfcefe4542cb3eaf34ce0212fdb2f9ece96782
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-07-23 14:35:09 +0600

    Add files via upload

--- avg.cpp
+++ avg.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-using std::endl;
 int main()
 {
     float A,B,avg,sum;
     cin>>A;
     cin>>B;
-    sum=A+B;
-    avg=float(sum/2);
+    sum=(A*3.5+B*7.5);
+    avg=float(sum/11);
     cout<<fixed<<setprecision(5);
-    cout<<"MEDIA  =  "<<avg<<endl;
+    cout<<"MEDIA = "<<avg<<endl;
     return 0;
 }
