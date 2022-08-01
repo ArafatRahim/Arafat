314462b916a2a7ebccc038699213907d3007808d
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-08-02 02:02:23 +0600

    Add files via upload

---
+++ Multiplication Table.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int x,n=1,i;
+cin>>x;
+for(i=1;i<=10;i++)
+{
+
+   int k=n*x;
+  cout<<i<<" x "<<x<<" = "<<k<<endl;
+  n++;
+}
+
+
+
+
+}
+
+
