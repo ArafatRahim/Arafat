dbd67a5e2ebca8ce082998026f55ba52bfd3d8f8
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-08-04 00:19:55 +0600

    Add files via upload

--- Sequence IJ 1.cpp
+++ Sequence IJ 1.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
-    int i,j;
-    for(i=1,j=60;i<=60&&j>=0;i+=3,j-=5)
-    cout<<"I="<<i<<" "<<"J="<<j<<endl;
+    int i,j,k;
+    for(k=1; i<9; k+=2)
+    {
+
+        for(i=k,j=7; i<=9&&j>=5; j--)
+        {
+            cout<<"I="<<i<<" "<<"J="<<j<<endl;
+
+        }
+    }
     return 0;
 }
