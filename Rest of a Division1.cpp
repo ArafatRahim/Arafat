4fc32208a074f5b805582652eaed71523a0f2636
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-08-07 20:23:40 +0600

    Add files via upload

---
+++ Rest of a Division1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int x,y,i,j;
+    cin>>x>>y;
+    for(i=0;i<y;i++)
+    {
+
+        if(i%5==2||i%5==3)
+            cout<<i<<endl;
+
+
+    }
+
+    return 0;
+
+}
