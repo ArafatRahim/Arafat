e517854124bc09f864fd2948823f11bc073c0229
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-08-18 23:27:31 +0600

    Add files via upload

---
+++ Summation from 1 to N.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    long long x,sum=0;
+    cin>>x;
+
+    if(x!=0)
+    {
+
+        sum=x*(x+1)/2;
+    }
+
+
+
+
+    cout<<sum<<endl;
+
+
+
+
+}
