fd3b8111167ce9bca36a180e224beec333c3e12c
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-08-18 20:17:13 +0600

    Add files via upload

---
+++ Basic Data Types.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main()
+{
+
+    int i;
+    long long ll;
+    char ch;
+    float f;
+    double d;
+
+
+    cin>>i;
+    cin>>ll;
+    cin>>ch;
+    cin>>f;
+    cin>>d;
+    cout<<i<<endl;
+    cout<<ll<<endl;
+    cout<<ch<<endl;
+    cout<<fixed<<setprecision(2)<<endl;
+    cout<<f<<endl;
+    cout<<fixed<<setprecision(1)<<endl;
+    cout<<d<<endl;
+
+
+
+
+}
