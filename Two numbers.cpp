a0155861cd801eb865fe7f6ea10d7fb02a03fec0
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-08-20 00:42:16 +0600

    Add files via upload

---
+++ Two numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+
+float x,y,div;
+int a,b;
+cin>>x>>y;
+
+
+cout<<"floor"<<" "<<x<<" "<<"/"<<" "<<y<<" "<<"="<<" "<<floor(x/y)<<endl;
+cout<<"ceil"<<" "<<x<<" "<<"/"<<" "<<y<<" "<<"="<<" "<<ceil(x/y)<<endl;
+cout<<"round"<<" "<<x<<" "<<"/"<<" "<<y<<" "<<"="<<" "<<round(x/y)<<endl;
+
+
+
+
+}
