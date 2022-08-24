17ca63564f57a6b1dea3181bbe9b2828811d78ea
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-08-24 16:51:38 +0600

    Add files via upload

---
+++ First digit !.cpp
@@ -0,0 +1,18 @@
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+
+     int x,y,z;
+     cin>>x;
+     y=x/1000;
+     if(y%2==0)
+     cout<<"EVEN";
+     else
+        cout<<"ODD";
+
+     return 0;
+
+     }
+
+
