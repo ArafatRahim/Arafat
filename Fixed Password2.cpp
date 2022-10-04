61d9b5526313b5b19effe4dfddc81e41e121c3cf
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-10-04 10:35:17 +0600

    Add files via upload

---
+++ Fixed Password2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,x;
+    int i;
+    while(1)
+    {
+        cin>>x;
+        if(x==1999)
+        {
+            cout<<"Correct"<<endl;
+            break;
+        }
+        else
+            cout<<"Wrong"<<endl;
+    }
+
+}
