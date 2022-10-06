891ff9b24b4d7a10ec7f9df5ef04ba0f1452a722
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-10-06 16:54:05 +0600

    Add files via upload

---
+++ Numbers Histogram.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    char a;
+    int n,i,b;
+    cin>>a;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+      cin>>b;
+      for(int j=1;j<=b;j++)
+      {
+          cout<<a;
+      }
+      cout<<endl;
+    }
+
+}
