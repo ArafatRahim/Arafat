719bf97a5f9fb020d8146972697e822cc59e653c
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-10-08 16:22:48 +0600

    Add files via upload

---
+++ Positions in array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>>
+using namespace std;
+int main()
+{
+    int x,a[10000],i,j,n;
+    cin>>x;
+    for(i=0; i<x; i++)
+    {
+        cin>>a[i];
+    }
+
+    for(i=0; i<x; i++)
+    {
+        if(a[i]<=10)
+            cout<<"A["<<i<<"]"<<" "<<"="<<" "<<a[i]<<endl;
+    }
+
+
+
+
+
+}
+
