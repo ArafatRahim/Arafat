656779d5e7f55002b482d45a3505f41e2943ab8f
ArafatRahim <109682644+ArafatRahim@users.noreply.github.com>
2022-10-10 22:31:18 +0600

    Add files via upload

---
+++ Q. Digits.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,n,i,j,a,b,t,c;
+    cin>>x;
+    for(i=0; i<x; i++)
+    {
+        cin>>n;
+        //if(n==0) cout<<n;
+
+        while(n!=0)
+        {
+            t= n%10;
+            n=n/10;
+            cout<<t<<" ";
+
+        }
+        cout<<endl;
+
+    }
+}
