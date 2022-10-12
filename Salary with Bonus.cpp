ef5ea48e963631dbe32f28c9b55be41a91de63b8
ARAFAT RAHIM <109682644+ArafatRahim@users.noreply.github.com>
2022-10-13 00:45:08 +0600

    Add files via upload

---
+++ Salary with Bonus.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string name;
+    double x,y,s;
+    cin>>name;
+    cin>>x>>y;
+    s=x+y*0.15;
+    cout<<fixed<<setprecision(2);
+    cout<<"TOTAL = R$ "<<s<<endl;
+
+}
