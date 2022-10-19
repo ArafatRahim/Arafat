3502a7dba96c3da1d0c32d7e2425641a7a7bf73f
ARAFAT RAHIM <109682644+ArafatRahim@users.noreply.github.com>
2022-10-20 01:48:45 +0600

    Add files via upload

--- A. Boy or Girl.cpp
+++ A. Boy or Girl.cpp
@@ -5,11 +5,19 @@ int main()
     int i,j,n=0;
     string a;
     cin>>a;
+    sort(a.begin(),a.end());
     for(i=0;i<a.size();i++)
+
     {
-        if(a[i]=a[i+1])
-           n++;
+        if(a[i]!=a[i+1])
+        n++;
     }
-    cout<<n;
+     if(n%2==0)
+        cout<<"CHAT WITH HER!"<<endl;
+     else cout<<"IGNORE HIM!"<<endl;
+
+
+
+
 
 }
