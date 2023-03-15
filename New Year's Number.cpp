336e21e76d6a89b221ef4f743f3f575ab1060388
ARAFAT RAHIM <109682644+ArafatRahim@users.noreply.github.com>
2023-03-16 01:32:20 +0600

    Add files via upload

---
+++ New Year's Number.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x;
+        cin>>x;
+
+        if(x/2020>=x%2020)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
